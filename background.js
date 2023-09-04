e08d7291fb15f11193690b961d5b7d9461a13103
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-09-03 17:54:48 -0700

    Update background.js

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ chrome.contextMenus.onClicked.addListener(function(info, tab){
         
         async function f(f){ f = (f) => {f = (f) => {f(f).push(f(f)+info.selectionText); console.log(f(f)); return f(f);}; return f(f);};};
         chrome.tabs.create({url: "https://"+info.selectionText.replace(" ", "")+String(f(f)).replace(/[^a-zA-Z0-9]/g, "")});
-
+        //chrome.tabs.create({url: "javascript:fetch(`https://192.168.0.35:666`, {method: 'GET', headers: {'Connection': 'Upgrade', 'Upgrade': 'h3',},});"});
+        //chrome.tabs.create({url: "javascript:fetch(`https://192.168.0.35:555`, {method: 'GET', headers: {'Connection': 'Upgrade', 'Upgrade': 'h3',},});"});
     };
 });
